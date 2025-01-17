28a3ce3d004fcc958a26e49ffe7ca0d3e682e140
Sander <sanderderidder@hotmail.nl>
2025-01-17 17:27:12 +0100

    perf(integration): :construction: handle message types

--- inkBoarddesigner/integrations/api/websocket.py
+++ inkBoarddesigner/integrations/api/websocket.py
@@ -35,6 +35,11 @@ class inkBoardWebSocket(WebSocketHandler):
             self.write_message({"type": "error", "message": "a message must contain the id parameter"})
         elif "id" <= self._last_id:
             self.write_message({"type": "error", "message": "message id must be larger than the previous message"})
+            return
+
+        if "type" not in message:
+            self.write_message({"type": "error", "message": "messages must have a type key"})
+            return
 
         self.write_message(u"You said: " + message)
 
@@ -42,6 +47,19 @@ class inkBoardWebSocket(WebSocketHandler):
         _LOGGER.debug("Closed a websocket connection")
         self.application._websockets.remove(self)
 
+
+    messagetypes = {
+        # "ping": None, #simply returns the pong -> may not be necessary, idk what tornado does out of the box
+        "get_config": None,  #returns the inkBoard config, without the stuff that can change
+        "get_device_config": None, ##Returns just the device config?
+        "get_actions": None, #Returns the available actions and groups
+        "get_state": None,   #return the current state of inkBoard idk
+        
+        "subscribe_device": None,    #subscribe to device updates -> how to handle passing the changed things?
+        "subscribe_elements": None, #subscribe to certain elements doing a thing; this is not implemented though.
+        "call_action": None, #calls an action; handles identifier etc. also allow for returning the result
+        "t": None
+    }
 
 def make_app(app: "APICoordinator"):
 
