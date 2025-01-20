7236924394e12cf7f3366392d1bd821adeddf108
Sander <sanderderidder@hotmail.nl>
2025-01-20 11:52:16 +0100

    feat(integration): :sparkles: Add touch watcher and a seperate message for sending out messages

--- inkBoarddesigner/integrations/api/websocket.py
+++ inkBoarddesigner/integrations/api/websocket.py
@@ -82,9 +82,137 @@ class inkBoardWebSocket(WebSocketHandler):
         self._watchers.add(watcher)
         return
 
-    watcher_types = ("watch_element", "watch_popups", "watch_device_feature")
+    def write_id_message(self, message_id : int, type_ : str, message : dict):
+        message["id"] = message_id
+        message["type"] = type_
+        return super().write_message(message)
 
-    messagetypes = {
+    def get_config(self, message_id : int):
+
+        return
+
+    async def watch_device_feature(self, message_id : int, feature : str):
+        """Notifies clients when the requested device feature has updated
+        """
+        try:
+            feature_str = FEATURES.get_feature_string(feature)
+        except AttributeError as exce:
+            self.write_id_message(message_id, "result", {"success": False,
+                                "message": str(exce)})
+            return
+
+        if not self.device.has_feature(feature_str):
+            self.write_id_message(message_id, "result", {"success": False,
+                    "message": f"Device does not have feature {feature_str} (passed as {feature})"})
+            return
+        
+        condition = self.device.get_feature_trigger(feature_str)
+        feature_state = self.device.get_feature_state(feature_str)
+        if not condition:
+            self.write_id_message(message_id, "result", {"success": False,
+                "message": f"Feature {feature} cannot be watched"})
+            return
+        
+        def test_state(feature_state):
+            current_state = self.device.get_feature_state(feature_str)
+            res = current_state != feature_state
+            if res:
+                return current_state
+            else:
+                return False
+
+        self.write_id_message(message_id, "result", {"success": True,
+            "message": f"Succesfully watching device feature {feature_str} from {feature}"})
+        while not self.close_code:
+            async with condition:
+                ##Can use the result from the predicate, so can try a custom function or something that returns False if no update
+                # await condition.wait_for(lambda: feature_state != self.device.get_feature_state(feature_str))
+                feature_state = await condition.wait_for(partial(test_state, feature_state))
+            
+            # feature_state = self.device.get_feature_state(feature_str)
+            self.write_id_message(message_id, "watch_device_feature",
+                                {"feature": feature_str, "feature_state": feature_state})
+        return
+
+    async def watch_popups(self, message_id : int):
+        """Notifies clients when the popups on top of the screen change
+        """
+        condition = self.screen.triggerCondition
+        self.write_id_message(message_id, "result", {"success": True})
+        while not self.close_code:
+            popup_list = self.screen.popupsOnTop.copy()
+            popup_strings = [popup.popupID for popup in self.screen.popupsOnTop if hasattr(popup, "popupID")]
+            self.write_id_message(message_id, "watch_popups", {"popups": popup_strings})
+            async with condition:
+                await condition.wait_for(lambda: popup_list != self.screen.popupsOnTop)
+        
+        _LOGGER.debug("stopped watching popups")
+
+    async def watch_element(self, message_id : int, element_id : str, properties : list[str]):
+        """Watches an element and notifies a client when the associated property has updated
+
+        Can be used to, for example, to be notified of a :py:class:`PythonScreenStackManager.elements.TabPages`` changing the page being shown.
+        """
+
+        ##Each update: write a dict with the new properties
+        try:
+            element = self.screen.elementRegister[element_id]
+            condition = element.triggerCondition
+            elt_dict = self._gather_element_properties(element, properties)
+            self.write_id_message(message_id, "result", {"success": True})
+        except KeyError:
+            self.write_id_message(message_id, "result", {"success": False, "message": f"No element with id {element_id}"})
+            return
+        except AttributeError as exce:
+            self.write_id_message(message_id, "result", {"success": False, "message": str(exce)})
+            return
+        except Exception:
+            self.write_id_message(message_id, "result", {"success": False, "message": str(exce)})
+            return
+        
+        while not self.close_code:
+            
+            async with condition:
+                await condition.wait_for(lambda: elt_dict != self._gather_element_properties(element, properties))
+
+            elt_dict = self._gather_element_properties(element, properties)
+            self.write_id_message(message_id, "watch_element",
+                        {"element_id": element_id,
+                        "properties": elt_dict
+                        })
+        return
+
+    async def watch_interaction(self, message_id: int):
+        """Notifies clients when an interaction event is dispatched
+        """
+
+        condition = self.screen.touchTrigger
+        self.write_id_message(message_id, "result",
+                            {"success": True})
+        
+        while not self.close_code:
+            await condition.await_trigger()
+            self.write_id_message(message_id, "watch_interaction",
+                            {"touch_event": self.screen.lastTouch._asdict()})
+
+    @staticmethod
+    def _gather_element_properties(element, property_list: list[str]) -> dict[str,Any]:
+
+        props = {}
+        for prop in property_list:
+            val = getattr(element, prop)
+            
+            if isinstance(val,set):
+                val = list(val)
+            elif isinstance(val,Element):
+                val = str(val)
+
+            props[prop] = val
+        return props
+
+    watcher_types = ("watch_element", "watch_popups", "watch_device_feature", "watch_interaction")
+
+messagetypes = {
         # "ping": None, #simply returns the pong -> may not be necessary, idk what tornado does out of the box
         "get_config": None,  #returns the inkBoard config, without the stuff that can change
         "get_device_config": None, ##Returns just the device config?
@@ -101,106 +229,6 @@ class inkBoardWebSocket(WebSocketHandler):
         "watch_popups": None,  #subscribe for registered popups being shown/removed
     }
 
-    async def watch_device_feature(self, message_id : int, feature : str):
-        
-        try:
-            feature_str = FEATURES.get_feature_string(feature)
-        except AttributeError as exce:
-            self.write_message({"id": message_id, "type": "result", "success": False,
-                                "message": str(exce)})
-            return
-
-        if not self.device.has_feature(feature_str):
-            self.write_message({"id": message_id, "type": "result", "success": False,
-                    "message": f"Device does not have feature {feature_str} (passed as {feature})"})
-            return
-        
-        condition = self.device.get_feature_trigger(feature_str)
-        feature_state = self.device.get_feature_state(feature_str)
-        if not condition:
-            self.write_message({"id": message_id, "type": "result", "success": False,
-                "message": f"Feature {feature} cannot be watched"})
-            return
-        
-        def test_state(feature_state):
-            current_state = self.device.get_feature_state(feature_str)
-            res = current_state != feature_state
-            if res:
-                return current_state
-            else:
-                return False
-
-        self.write_message({"id": message_id, "type": "result", "success": True,
-            "message": f"Succesfully watching device feature {feature_str} from {feature}"})
-        while not self.close_code:
-            async with condition:
-                ##Can use the result from the predicate, so can try a custom function or something that returns False if no update
-                # await condition.wait_for(lambda: feature_state != self.device.get_feature_state(feature_str))
-                feature_state = await condition.wait_for(partial(test_state, feature_state))
-            
-            # feature_state = self.device.get_feature_state(feature_str)
-            self.write_message({"id": message_id, "type": "watch_device_feature",
-                                "feature": feature_str, "feature_state": feature_state})
-        return
-
-    async def watch_popups(self, message_id : int):
-        
-        condition = self.screen.triggerCondition
-        self.write_message({"id": message_id, "type": "result", "success": True})
-        while not self.close_code:
-            popup_list = self.screen.popupsOnTop.copy()
-            popup_strings = [popup.popupID for popup in self.screen.popupsOnTop if hasattr(popup, "popupID")]
-            self.write_message({"id": message_id, "type": "watch_popups", "popups": popup_strings})
-            async with condition:
-                await condition.wait_for(lambda: popup_list != self.screen.popupsOnTop)
-        
-        _LOGGER.debug("stopped watching popups")
-
-    async def watch_element(self, message_id : int, element_id : str, properties : list[str]):
-
-        ##Each update: write a dict with the new properties
-        try:
-            element = self.screen.elementRegister[element_id]
-            condition = element.triggerCondition
-            elt_dict = self._gather_element_properties(element, properties)
-            self.write_message({"id": message_id, "type": "result", "success": True})
-        except KeyError:
-            self.write_message({"id": message_id, "type": "result", "success": False, "message": f"No element with id {element_id}"})
-            return
-        except AttributeError as exce:
-            self.write_message({"id": message_id, "type": "result", "success": False, "message": str(exce)})
-            return
-        except Exception:
-            self.write_message({"id": message_id, "type": "result", "success": False, "message": str(exce)})
-            return
-        
-        while not self.close_code:
-            
-            async with condition:
-                await condition.wait_for(lambda: elt_dict != self._gather_element_properties(element, properties))
-
-            elt_dict = self._gather_element_properties(element, properties)
-            self.write_message({"id": message_id, 
-                        "type": "watch_element",
-                        "element_id": element_id,
-                        "properties": elt_dict
-                        })
-        return
-
-    @staticmethod
-    def _gather_element_properties(element, property_list: list[str]) -> dict[str,Any]:
-
-        props = {}
-        for prop in property_list:
-            val = getattr(element, prop)
-            
-            if isinstance(val,set):
-                val = list(val)
-            elif isinstance(val,Element):
-                val = str(val)
-
-            props[prop] = val
-        return props
 
 def make_app(app: "APICoordinator"):
 
