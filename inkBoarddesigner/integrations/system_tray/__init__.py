ae9a32316089224eec5925563705a911165fe57a
Sander <sanderderidder@hotmail.nl>
2025-02-02 12:51:19 +0100

    feat(integration): :sparkles: Let the integration automatically position the window along the system tray

--- inkBoarddesigner/integrations/system_tray/__init__.py
+++ inkBoarddesigner/integrations/system_tray/__init__.py
@@ -9,6 +9,8 @@ if TYPE_CHECKING:
     from .trayicon import TrayIcon
 
 _LOGGER = inkBoard.getLogger(__name__)
+TRAYSIZE = 50   #Traysize in pixel
+TOLERANCE = 1.1 #Small tolerance multiplier to enlarge the identification of clicks on the tray correctly
 
 async def async_setup(core: "CORE", config: "config"):
 
@@ -39,3 +41,6 @@ class system_tray_entry(TypedDict):
 
     toolwindow: bool = False
     "Hides inkBoard from the taskbar entirely. Opening and closing the window can be done via the taskbar icon."
+
+    tray_size: int = TRAYSIZE
+    "Size of the system tray, in pixels. Used when using the auto_position option"
--- inkBoarddesigner/integrations/system_tray/trayicon.py
+++ inkBoarddesigner/integrations/system_tray/trayicon.py
@@ -12,7 +12,7 @@ import inkBoard
 from inkBoard.constants import INKBOARD_ICON
 from inkBoard.helpers import QuitInkboard
 
-from . import system_tray_entry
+from . import system_tray_entry, TRAYSIZE, TOLERANCE
 
 if TYPE_CHECKING:
     from inkBoard import core as CORE, config
@@ -26,6 +26,7 @@ class HIDEACTIONS:
     ICONIFY = "iconify"
     WITHDRAW = "withdraw"
 
+
 class TrayIcon(pystray.Icon):
 
     def __init__(self, core: "CORE", config: "config", **kwargs):
@@ -35,7 +36,6 @@ class TrayIcon(pystray.Icon):
             tray_config = default_config
         else:
             tray_config = default_config.copy() | tray_config
-
 
         if tray_config.get("hide_window", False):
             if core.DESIGNER_RUN:
@@ -48,6 +48,8 @@ class TrayIcon(pystray.Icon):
         ##Other options to add: custom icon; custom name
 
         self._toolwindow = tray_config.get("toolwindow", False)
+
+        self._tray_size = tray_config.get("tray_size", TRAYSIZE)
 
         if tray_config["icon"] == "circle":
             imgfile = Path(__file__).parent / "files" / f"icon_circle.ico"
@@ -88,18 +90,83 @@ class TrayIcon(pystray.Icon):
         "Minimises the window. Must be called in the main thread"
         _LOGGER.debug(f"Minimising window via {item}")
 
+        self.window.update()
+        x = self.window.winfo_pointerx()
+        y = self.window.winfo_pointery()
+        # s = self.window.wm_state()
+        
+
         if self.window.wm_state() != "normal":
             if self._minimise_action == HIDEACTIONS.WITHDRAW:
                 ##This simply makes the animation of the window appearing a lot smoother
                 self.window.iconify()
             self.window.deiconify()
+            self._set_window_position(x,y)
         else:
             if self._minimise_action == HIDEACTIONS.ICONIFY:
                 self.window.withdraw()
             else:
                 self.window.iconify()
-        
+
         self.window.update()
+
+    def _set_window_position(self, x, y):
+
+        ##5 types of locations: top, right, bottom, left, middle
+
+        window = self.window
+
+        w = self.window.winfo_width()
+        h = self.window.winfo_height()
+
+        win_x = None
+        win_y = None
+
+        (sw, sh) = (window.winfo_screenwidth(), window.winfo_screenheight())
+        _LOGGER.verbose(f"Clicked on {(x,y)}, screen size is {(sw, sh)}")
+
+        if x >= sw - TOLERANCE*self._tray_size:
+            ##Should be on the right of screen
+            win_x = sw - w - self._tray_size
+        elif x <= TOLERANCE*self._tray_size:
+            ##Tray is on the left of screen
+            win_x = self._tray_size
+
+        if y >= sh - TOLERANCE*self._tray_size:
+            ##Tray is on bottom of screen
+            win_y = sh - self._tray_size - h
+        elif y <= TOLERANCE*self._tray_size:
+            ##Tray is on the top of screen
+            win_y = self._tray_size
+
+        if win_x == None and win_y == None:
+            _LOGGER.error(f"Cannot determine location of the system tray from click coordinates {(x,y)}")
+            return
+        elif win_y == None and win_x != None:
+                cy = y - 2*self._tray_size
+                if cy <= 0:
+                    ##align window to top
+                    win_y = 0
+                elif cy + h >= sh:
+                    ##Align window to bottom
+                    win_y = sh - h
+                else:
+                    win_y = cy
+        elif win_x == None and win_y != None:
+            cx = x - 2*self._tray_size
+            if cx <= 0:
+                ##align window to top
+                win_x = 0
+            elif cx + w >= sw:
+                ##Align window to bottom
+                win_x = sw - w
+            else:
+                win_x = cx
+
+
+        new_geo = f"{w}x{h}+{win_x}+{win_y}"
+        _LOGGER.verbose(f"Repositioning window via tray location to {(win_x,win_y)}")
+        self.window.wm_geometry(new_geo)
 
     def _reload_inkboard(self, item):
         self.__core.screen.reload()
